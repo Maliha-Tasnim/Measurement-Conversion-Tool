28cd163030334049f2abe4f94f3bfe606ff8aaf8
Maliha-Tasnim <tmaliha94@gmail.com>
2018-11-27 23:19:16 +0600

    Add existing file

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Measurement Conversion</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <style>
+    body {
+    background-color: darkblue;
+}
+</style>
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1 style="color:white;"><center>Measurement Conversion</h1>
+      <center>
+      <ul id="measurement-types">
+        <li><a href="length.php">Length and distance</a></li>
+        <li><a href="area.php">Area</a></li>
+        <li><a href="volume.php">Volume and capacity</a></li>
+        <li><a href="mass.php">Mass and weight</a></li>
+        <li><a href="speed.php">Speed</a></li>
+        <li><a href="temperature.php">Temperature</a></li>
+      </ul>
+      </center>
+    </div>
+  </body>
+</html>
---
+++ temperature.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once('includes/functions.php');
+
+$from_value = '';
+$from_unit = '';
+$to_unit = '';
+$to_value = '';
+
+if(isset($_POST['submit'])){
+  $from_value = $_POST['from_value'];
+  $from_unit = $_POST['from_unit'];
+  $to_unit = $_POST['to_unit'];
+  
+  $to_value = convert_temperature($from_value, $from_unit, $to_unit);
+}
+
+$temp_options = array(
+  "Celsius",
+  "Fahrenheit",
+  "Kelvin"
+);
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Convert Temperature</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+    <style>
+    body {
+    background-color: darkblue;
+}
+</style>
+  </head>
+  <body>
+
+    <div id="main-content">
+
+      <h1 style="color:white;"><center>Convert Temperature</h1>
+      <center>
+      <form action="" method="post">
+        
+        <div class="entry">
+          <label style="color:white;">From:</label>&nbsp;
+          <input type="text" name="from_value" value="<?php echo $from_value; ?>" />&nbsp;
+          <select name="from_unit">
+            <?php
+            foreach($temp_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($from_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+        </div>
+        
+        <div class="entry">
+          <label style="color:white;">To:</label>&nbsp;
+          <input type="text" name="to_value" value="<?php echo float_to_string($to_value); ?>" />&nbsp;
+          <select name="to_unit">
+            <?php
+            foreach($temp_options as $unit) {
+              $opt = optionize($unit);
+              echo "<option value=\"{$opt}\"";
+              if($to_unit == $opt) { echo " selected"; }
+              echo ">{$unit}</option>";
+            }
+            ?>
+          </select>
+          
+        </div>
+        
+        <input type="submit" name="submit" value="Submit" />
+      </form>
+  
+      <br />
+      <a href="index.php" style="color:white;">Return to menu</a>
+      </center>
+    </div>
+  </body>
+</html>
